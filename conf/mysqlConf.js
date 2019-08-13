ea923a1305124730ec845be901a567d683b6d762
tranminhhuydn <tranminhhuydn@gmail.com>
2019-08-13 17:04:54 +0700

    Update mysqlConf.js

--- conf/mysqlConf.js
+++ conf/mysqlConf.js
@@ -1,10 +1,8 @@
 module.exports = {
     mysql: {
-        host: process.env.MYSQL_ADDON_HOST,
-        user: process.env.MYSQL_ADDON_USER,
-        password: process.env.MYSQL_ADDON_PASSWORD,
-        database: process.env.MYSQL_ADDON_DB,
-        // 最大连接数，默认为10
-        connectionLimit: 10
+    	host     : process.env.MYSQL_ADDON_HOST,
+    	database : process.env.MYSQL_ADDON_DB,
+    	user     : process.env.MYSQL_ADDON_USER,
+    	password : process.env.MYSQL_ADDON_PASSWORD
     }
 };
